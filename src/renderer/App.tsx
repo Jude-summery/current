2242c52d51e1952fcb6c0b897b1740736f38313e
Jude-summery <329543240@qq.com>
2022-06-18 14:29:36 +0800

    update: 引入MUI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,19 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import { CssBaseline } from '@mui/material';
+import Contacts from './Contacts';
+import Chat from './Chat';
 
-const Hello = () => {
+const Layout = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <CssBaseline />
+      <div>
+        <div>
+          <Contacts />
+        </div>
+        <div>
+          <Chat />
+        </div>
       </div>
     </div>
   );
@@ -43,7 +23,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Layout />} />
       </Routes>
     </Router>
   );
